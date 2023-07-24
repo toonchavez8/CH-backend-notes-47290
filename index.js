3bfcb8508b069739ceed391ea8092bfc8546573c
Miguel Chavez <toonchavez8@gmail.com>
2023-07-24 17:51:18 -0600

    moving consolelogs

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // All imports
 import chalk from "chalk";
-import longestWord from "./clase-0/isReverseInt";
-import isReverseInt from "./clase-0/isReverseInt.js";
 
 // loading console log
 console.log(chalk.yellow("starting ...   "));
-
-console.log(longestWord("Hello my name is John"));
-
-console.log(longestWord("The quick brown fox jumps over the lazy dog"));
-
-console.log(isReverseInt(123, 321)); // true
-console.log(isReverseInt(-123, -321)); // true
-console.log(isReverseInt(123, 123)); // false
-console.log(isReverseInt(-123, 321)); // false

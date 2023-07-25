53d3cfe680b1080527ed23fd4dccae2ce2c2f2be
Miguel Chavez <toonchavez8@gmail.com>
2023-07-24 19:58:44 -0600

    realizando entrega de primer desafio

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 // All imports
 import chalk from "chalk";
-import { PM } from "./clase-02-desafio/Productmanager.js";
+import { PM } from "./ProductManager.js";
 
 // loading console log
 console.log(chalk.yellow("starting ...   "));
-
-PM.addProduct(
-	"Samsung S20",
-	"Smartphone",
-	1200,
-	"samsung-s20.jpg",
-	"S20-01",
-	50
-);
 
 console.log("products", PM.getProducts());

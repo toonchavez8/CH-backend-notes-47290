95b5647e5242502b60007e5048a48f7f5b73124b
Miguel Chavez <toonchavez8@gmail.com>
2023-08-13 09:54:00 -0600

    add users id via url paramas

--- clase-06/index.js
+++ clase-06/index.js
@@ -3,6 +3,20 @@ import chalk from "chalk";
 // 01 import express from express
 import express from "express";
 
+const users = [
+	{
+		id: 1,
+		name: "John",
+		last: "snow",
+		age: 30,
+	},
+	{
+		id: 2,
+		name: "Jane",
+		last: "Doe",
+		age: 25,
+	},
+];
 // 02 declare express app
 const app = express();
 
@@ -11,7 +25,18 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 app.get("/users", (req, res) => {
-	res.send("User List");
+	res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+	// get id from url params
+	const id = parseInt(req.params.id);
+	// find user by id
+	const user = users.find((user) => user.id === id);
+	// if no user found, respond with 404
+	if (!user) return res.status(404).send("User not found");
+	// else return user
+	res.json(user);
 });
 
 // 03 define port and start server

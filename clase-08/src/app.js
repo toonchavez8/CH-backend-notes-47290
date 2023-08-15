dc8d63f700652d966c0814b0a45d642c4fb52681
Miguel Chavez <toonchavez8@gmail.com>
2023-08-14 19:31:56 -0600

    use routers and add db

---
+++ clase-08/src/app.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+import express from "express";
+import router from "./router/products.router.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json("home page");
+});
+
+app.use("/products", router);
+
+app.listen(3000, () => {
+	console.log(chalk.green("server up"));
+});

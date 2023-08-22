fe9648ae8b8c8079a60e57d9431bbaedffa4f5d7
Miguel Chavez <toonchavez8@gmail.com>
2023-08-21 22:02:33 -0600

    add cart by id route

--- src/managers/cartManager.js
+++ src/managers/cartManager.js
@@ -1,27 +1,27 @@
 import fs from "fs";
 import chalk from "chalk";
+import { ProductManager } from "./ProductManager.js";
 
+const productmanager = new ProductManager("./data/database.json");
 export class CartManager {
 	#_cart;
-	#productDBFilePath;
 	#cartDBFilePath;
 
-	constructor(productDBFilePath, cartDBFilePath) {
+	constructor(cartDBFilePath) {
 		this.#_cart = [];
 
-		this.#productDBFilePath = productDBFilePath;
 		this.#cartDBFilePath = cartDBFilePath;
 
 		if (cartDBFilePath) {
-			this.loadCartDatabase(cartDBFilePath);
+			this.#loadCartDatabase(cartDBFilePath);
 		}
 	}
 
-	async loadCartDatabase(cartDBFilePath) {
+	async #loadCartDatabase(cartDBFilePath) {
 		try {
 			// Attempt to read the cart database file
 			const data = await fs.promises.readFile(cartDBFilePath, "utf-8");
-			return JSON.parse(data);
+			this.#_cart = JSON.parse(data);
 		} catch (error) {
 			if (error.code === "ENOENT") {
 				// The file doesn't exist, so create an empty cart database
@@ -41,7 +41,7 @@ export class CartManager {
 		}
 	}
 
-	async saveCartDatabase() {
+	async #saveCartDatabase() {
 		try {
 			await fs.promises.writeFile(
 				this.#cartDBFilePath,
@@ -49,6 +49,65 @@ export class CartManager {
 			);
 		} catch (error) {
 			console.error("Error writing to cart database:", error);
+		}
+	}
+
+	#generateId(data) {
+		return data.length === 0 ? 1 : data[data.length - 1].id + 1;
+	}
+
+	async createCart() {
+		const cartToAdd = { id: this.#generateId(this.#_cart), products: [] };
+
+		this.#_cart.push(cartToAdd);
+
+		try {
+			await this.#saveCartDatabase();
+		} catch (error) {
+			throw new Error(`Error creating cart: ${error.message}`);
+		}
+
+		return cartToAdd;
+	}
+
+	async getProductsFromCart(cid) {
+		console.log(cid);
+		const cart = this.#_cart.find((cart) => cart.id === cid);
+
+		if (!cart) {
+			throw new Error(`Cart with ID ${cid} not found.`);
+		}
+
+		return cart;
+	}
+	async addProductToCart(cid, pid) {
+		try {
+			const product = await productmanager.getProductbyId(pid);
+
+			if (!product) {
+				throw new Error(`Product with ID ${pid} not found.`);
+			}
+
+			const cart = this.#_cart.find((cart) => cart.id === cid);
+
+			if (!cart) {
+				throw new Error(`Cart with ID ${cid} not found.`);
+			}
+
+			const existingProduct = cart.products.find((p) => p.id === pid);
+
+			if (existingProduct) {
+				existingProduct.quantity = (existingProduct.quantity || 0) + 1;
+			} else {
+				product.quantity = 1;
+				cart.products.push(product);
+			}
+
+			await this.#saveCartDatabase();
+
+			return cart;
+		} catch (error) {
+			throw new Error(`Error adding product to cart: ${error.message}`);
 		}
 	}
 }
--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,30 +1,44 @@
 import { Router } from "express";
 import { CartManager } from "../managers/cartManager.js";
 import chalk from "chalk";
+import router from "./product.router.js";
 
 const cartRouter = Router();
-const productDBFilePath = "./data/database.json";
 const cartDBFilePath = "./data/cartdb.json";
 
-const cartManager = new CartManager(productDBFilePath, cartDBFilePath);
+const cartManager = new CartManager(cartDBFilePath);
 
 cartRouter.post("/", async (req, res) => {
 	try {
-		// Extract data from the request body
-		const { products } = req.body;
-
-		// Create a new cart object with a unique ID
-		const newCart = {
-			products: products || [], // Initialize products as an empty array or use the provided products if available
-		};
-
-		// Add the new cart to the cart database
-		const addedCart = await cartManager.addToCart(newCart);
+		// Create a new cart
+		const newCart = await cartManager.createCart();
 
 		// Respond with the newly created cart
-		res.status(201).json(addedCart);
+		res.status(201).json(newCart);
 	} catch (error) {
 		console.error(chalk.red("Error creating cart:", error.message));
+		res.status(500).json({ error: error.message });
+	}
+});
+
+cartRouter.get("/:cid", async (req, res) => {
+	try {
+		const id = parseInt(req.params.cid);
+
+		// Call the getProductsFromCart method to retrieve products from the cart
+		const result = await cartManager.getProductsFromCart(id);
+
+		if (!result) {
+			// Handle the case where the cart or products are not found
+			console.log(result);
+			res.status(404).json({ error: `Cart with ID ${id} not found.` });
+		} else {
+			// Respond with the retrieved products
+			res.status(200).json({ status: "success", payload: result });
+		}
+	} catch (error) {
+		// Handle any other errors that occur during the request
+		console.error("Error retrieving products from cart:", error.message);
 		res.status(500).json({ error: error.message });
 	}
 });

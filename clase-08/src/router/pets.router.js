c17f288ebd694186f5efc3d8f1ef1c6da2a740bf
Miguel Chavez <toonchavez8@gmail.com>
2023-08-28 19:54:30 -0600

    middleware testing

--- clase-08/src/router/pets.router.js
+++ clase-08/src/router/pets.router.js
@@ -3,8 +3,21 @@ import { Router } from "express";
 const router = Router();
 const pets = [];
 
+//middle ware
+
+const petsActive = false;
+
+const allowNewPets = (req, res, next) => {
+	if (!petsActive)
+		return res.status(500).json({
+			status: "error",
+			error: "its not time to add pets",
+		});
+	next();
+};
+
 router.get("/", (req, res) => res.json({ pets }));
-router.post("/", (req, res) => {
+router.post("/", allowNewPets, (req, res) => {
 	const pet = req.body;
 	pet.id = pets.length === 0 ? 1 : pets[pets.length - 1].id + 1;
 	pets.push(pet);

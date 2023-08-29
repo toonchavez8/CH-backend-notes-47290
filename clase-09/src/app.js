c374e6e052ca488b69f7c43accedf2358adf5175
Miguel Chavez <toonchavez8@gmail.com>
2023-08-28 22:32:36 -0600

    refactor route to use router forlder

--- clase-09/src/app.js
+++ clase-09/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import chalk from "chalk";
 import handlebars from "express-handlebars";
+import userRouter from "./routers/users.router.js";
 
 const app = express();
 
@@ -11,9 +12,7 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", "../src/views");
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-	res.render("home", { user: "miguel" });
-});
+app.use("/users", userRouter);
 
 app.listen(port, () => {
 	console.log(chalk.green(`Sever up and running on ${port}!`));

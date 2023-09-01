5dea3e5134b0d9d05a2c2a50934f3c130a486bc6
Miguel Chavez <toonchavez8@gmail.com>
2023-08-31 23:20:53 -0600

    setup realtime products and tables for products page

--- src/app.js
+++ src/app.js
@@ -2,18 +2,40 @@ import chalk from "chalk";
 import express from "express";
 import productRouter from "./routers/product.router.js";
 import cartRouter from "./routers/cart.router.js";
+import { Server } from "socket.io";
+import handlebars from "express-handlebars";
+import viewsRouter from "./routers/view.router.js";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(express.static("./src/public"));
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
 
+// Routes
 app.get("/", (req, res) => {
-	res.json("home page");
+	res.render("index");
 });
-
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
+app.use("/products", viewsRouter);
 
-app.listen(3000, () => {
-	console.log(chalk.green("server up"));
+// Error handling middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something went wrong!");
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+	console.log(chalk.green(`Server connected to port ${port}`))
+);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+	console.log(chalk.yellow("New client connected"));
 });
---
+++ src/routers/view.router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { ProductManager } from "../managers/ProductManager.js";
+
+const viewsRouter = Router();
+const productmanager = new ProductManager("./data/database.json");
+
+viewsRouter.get("/", async (req, res) => {
+	const products = await productmanager.getProducts();
+	res.render("home", { products });
+	console.log(products);
+});
+
+viewsRouter.get("/realtimeproducts", async (req, res) => {
+	const products = await productmanager.getProducts();
+	res.render("realTimeProducts", { products });
+});
+
+export default viewsRouter;

5dcf3fc0aa2476774b2ea7dd056e5659944dbba8
Miguel Chavez <toonchavez8@gmail.com>
2023-09-28 21:07:19 -0600

    this commit will connect to db with mongoose

--- clase-14/index.js
+++ clase-14/index.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import mongoose from "mongoose";
 
 // We create data for our DB
@@ -24,3 +25,24 @@ const personajesGOTSchema = new mongoose.Schema({
 // DAO = Data Access Object
 // we tell mongose to create a new collection with the shape of our shema
 const personajesGOTDAO = mongoose.model("personajesGOT", personajesGOTSchema);
+
+// try catch finaly block that connects to mongoDB via mongoose
+try {
+	// we await a connection with mongoose
+	await mongoose.connect(
+		"mongodb+srv://toonchavez8:Iac3b3br.@cluster0.aotpgnu.mongodb.net/",
+		{
+			dbName: "personajesGOT",
+		}
+	);
+
+	// for each personajeGot we create a new document useing the DAO
+	for (const personajeGOT of personajesGOT) {
+		await personajesGOTDAO.create(personajeGOT);
+	}
+	console.log(chalk.green("db Connected"));
+} catch (error) {
+	console.log(chalk.red(error.message));
+} finally {
+	console.log(chalk.yellow("await request successfull"));
+}

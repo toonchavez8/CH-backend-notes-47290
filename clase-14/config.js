c5f5e33d130bce192daa92fbe338f5d46c097592
Miguel Chavez <toonchavez8@gmail.com>
2023-09-28 21:28:23 -0600

    refactor mongoose connection with funcitosn

---
+++ clase-14/config.js
@@ -0,0 +1,5 @@
+export default {
+	// MongoDB connection URI (replace with your actual URI)
+
+	mongoURI: "mongodb+srv://toonchavez8:Iac3b3br.@cluster0.aotpgnu.mongodb.net/",
+};
--- clase-14/index.js
+++ clase-14/index.js
@@ -1,48 +1,62 @@
 import chalk from "chalk";
 import mongoose from "mongoose";
+import config from "./config.js";
 
-// We create data for our DB
-const personajesGOT = [
-	{ nombre: "Jon Snow", edad: 30, nota: 9 },
-	{ nombre: "Daenerys Targaryen", edad: 25, nota: 8 },
-	{ nombre: "Tyrion Lannister", edad: 40, nota: 9 },
-	{ nombre: "Arya Stark", edad: 20, nota: 8 },
-	{ nombre: "Sansa Stark", edad: 25, nota: 8 },
-	{ nombre: "Cersei Lannister", edad: 45, nota: 7 },
-	{ nombre: "Jaime Lannister", edad: 40, nota: 8 },
-	{ nombre: "Bran Stark", edad: 22, nota: 9 },
-	{ nombre: "Margaery Tyrell", edad: 28, nota: 9 },
-	{ nombre: "Robb Stark", edad: 30, nota: 8 },
-];
-
-// we tell mongoose the schema shape of our collection
-const personajesGOTSchema = new mongoose.Schema({
-	nombre: String,
-	edad: Number,
-	nota: Number,
-});
-
-// DAO = Data Access Object
-// we tell mongose to create a new collection with the shape of our shema
-const personajesGOTDAO = mongoose.model("personajesGOT", personajesGOTSchema);
-
-// try catch finaly block that connects to mongoDB via mongoose
-try {
-	// we await a connection with mongoose
-	await mongoose.connect(
-		"mongodb+srv://toonchavez8:Iac3b3br.@cluster0.aotpgnu.mongodb.net/",
-		{
+// we create a function that will create the connection to the db using a config file
+async function connectToDatabase() {
+	try {
+		await mongoose.connect(config.mongoURI, {
 			dbName: "personajesGOT",
-		}
-	);
-
-	// for each personajeGot we create a new document useing the DAO
-	for (const personajeGOT of personajesGOT) {
-		await personajesGOTDAO.create(personajeGOT);
+		});
+		console.log(chalk.green("db Connected"));
+	} catch (error) {
+		console.log(chalk.red(error.message));
+		throw error;
 	}
-	console.log(chalk.green("db Connected"));
-} catch (error) {
-	console.log(chalk.red(error.message));
-} finally {
-	console.log(chalk.yellow("await request successfull"));
 }
+
+// then we pop db after connecting with mock data
+async function populateDatabase() {
+	let errorOccured = false;
+	try {
+		await connectToDatabase();
+
+		const personajesGOT = [
+			{ nombre: "Jon Snow", edad: 30, nota: 9 },
+			{ nombre: "Daenerys Targaryen", edad: 25, nota: 8 },
+			{ nombre: "Tyrion Lannister", edad: 40, nota: 9 },
+			{ nombre: "Arya Stark", edad: 20, nota: 8 },
+			{ nombre: "Sansa Stark", edad: 25, nota: 8 },
+			{ nombre: "Cersei Lannister", edad: 45, nota: 7 },
+			{ nombre: "Jaime Lannister", edad: 40, nota: 8 },
+			{ nombre: "Bran Stark", edad: 22, nota: 9 },
+			{ nombre: "Margaery Tyrell", edad: 28, nota: 9 },
+			{ nombre: "Robb Stark", edad: 30, nota: 8 },
+		];
+
+		const personajesGOTSchema = new mongoose.Schema({
+			nombre: String,
+			edad: Number,
+			nota: Number,
+		});
+
+		const personajesGOTDAO = mongoose.model(
+			"personajesGOT",
+			personajesGOTSchema
+		);
+
+		// Use a insertMany to create documents
+		await personajesGOTDAO.insertMany(personajesGOT);
+	} catch (error) {
+		console.log(chalk.red(error.message));
+		errorOccured = true;
+	} finally {
+		if (errorOccured) {
+			console.log(chalk.yellow("Error occurred during await request."));
+		} else {
+			console.log(chalk.green("await request successful"));
+		}
+	}
+}
+
+populateDatabase();

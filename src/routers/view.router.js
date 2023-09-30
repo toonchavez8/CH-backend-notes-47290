25d6a3c7df5bb449a39d4adc9b61f411ef9f048d
Miguel_Chavez <toonchavez8@gmail.com>
2023-09-30 11:09:46 -0600

    update view to use mongo db

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -1,18 +1,30 @@
 import { Router } from "express";
 import { ProductManager } from "../dao/managers/ProductManager.js";
+import productModel from "../dao/models/products.model.js";
 
 const viewsRouter = Router();
 const productmanager = new ProductManager("./data/database.json");
 
 viewsRouter.get("/", async (req, res) => {
-	const products = await productmanager.getProducts();
+	const products = await productModel.find().lean().exec();
+
 	res.render("home", { products });
 	console.log(products);
 });
 
 viewsRouter.get("/realtimeproducts", async (req, res) => {
-	const products = await productmanager.getProducts();
-	res.render("realTimeProducts", { products });
+	try {
+		const products = await productModel.find().lean().exec();
+		const processedProducts = products.map((product) => ({
+			...product,
+			shortId: product._id.toString().slice(-4),
+		}));
+		res.render("realTimeProducts", { processedProducts });
+	} catch (error) {
+		// Handle any potential errors here
+		console.error("Error:", error);
+		res.status(500).send("Internal Server Error");
+	}
 });
 
 export default viewsRouter;

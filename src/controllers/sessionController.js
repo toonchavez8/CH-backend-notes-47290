9116b317b61183a35cf13c86edb2944adf8b3535
Miguel Chavez <toonchavez8@gmail.com>
2023-10-30 20:16:27 -0600

    migrating sessions to controllers

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,18 @@
+import { JWT_COOKIE_NAME } from "../utils.js";
+
+export const register = async (req, res) => {
+	console.log("registering user");
+
+	res.redirect("/session/login");
+};
+
+export const login = async (req, res) => {
+	if (!req.user) {
+		return res.status(401).render("error", { error: "passport login failed" });
+	}
+	res.cookie(JWT_COOKIE_NAME, req.user.token).redirect("/products");
+};
+
+export const logout = (req, res) => {
+	res.clearCookie(JWT_COOKIE_NAME).redirect("/session/login");
+};
--- src/routers/sessions.router.js
+++ src/routers/sessions.router.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-
 import passport from "passport";
-import { JWT_COOKIE_NAME } from "../utils.js";
+import * as sessionController from "../controllers/sessionController.js";
 
 const sessionsRouter = Router();
 
@@ -10,28 +9,13 @@ sessionsRouter.post(
 	passport.authenticate("register", {
 		failureRedirect: "/failregister",
 	}),
-	async (req, res) => {
-		console.log("REQ SESSION from register", req.user);
-		console.log("req.token", req.token);
-		res.redirect("/session/login");
-	}
+	sessionController.register
 );
-
 sessionsRouter.post(
 	"/login",
 	passport.authenticate("login", { failureRedirect: "/failregister" }),
-	async (req, res) => {
-		if (!req.user) {
-			return res
-				.status(401)
-				.render("error", { error: "passport login failed" });
-		}
-		res.cookie(JWT_COOKIE_NAME, req.user.token).redirect("/products");
-	}
+	sessionController.login
 );
-
-sessionsRouter.get("/logout", (req, res) => {
-	res.clearCookie(JWT_COOKIE_NAME).redirect("/session/login");
-});
+sessionsRouter.get("/logout", sessionController.logout);
 
 export default sessionsRouter;

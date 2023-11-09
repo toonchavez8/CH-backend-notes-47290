90bb70157ebd2aa4b1d78551a037b739f9aa4006
Miguel Chavez <toonchavez8@gmail.com>
2023-11-08 20:34:25 -0600

    clase 16 notes

--- clase-16/src/index.js
+++ clase-16/src/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import userModel from "./models/user.model.js";
+import studentModel from "./models/student.model.js";
+import courseModel from "./models/course.model.js";
 
 const uri = "mongodb://localhost:27017";
 
@@ -9,9 +10,24 @@ try {
 	});
 	console.log("Connected to MongoDB");
 
-	const response = await userModel
-		.find({ first_name: "Hyacinth" })
-		.explain("executionStats");
+	const Courseresponse = await courseModel.create({
+		title: "Curso de Node.js",
+		description: "Curso de Node.js en Platzi",
+		difficulty: 1,
+		proffesor: "Doc OC",
+	});
+
+	const response = await studentModel.create({
+		first_name: "Juan",
+		last_name: "Perez",
+		email: "jperez@gmail.com",
+		courses: [
+			{
+				course: Courseresponse._id,
+			},
+		],
+	});
+	console.log(Courseresponse);
 	console.log(response);
 } catch (error) {
 	console.log("Error connecting to MongoDB");
--- clase-16/src/models/student.model.js
+++ clase-16/src/models/student.model.js
@@ -3,6 +3,18 @@ import mongoose from "mongoose";
 const studentSchema = mongoose.Schema({
 	first_name: String,
 	last_name: String,
+	email: String,
+	courses: {
+		type: [
+			{
+				course: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Course",
+				},
+				_id: false,
+			},
+		],
+	},
 });
 
 const Student = mongoose.model("Student", studentSchema);

a090ddcbe603a520cdb730ea42960abbf8d4b06a
Miguel Chavez <toonchavez8@gmail.com>
2023-11-11 17:37:49 -0600

    setting up switch for seession | setup fails

--- clase-18/src/app.js
+++ clase-18/src/app.js
@@ -1,9 +1,28 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
-app.use(cookieParser("secret"));
+const storageMethod = "cookie";
+
+// Middleware to set up storage based on the chosen method
+const setupStorage = (req, res, next) => {
+	if (storageMethod === "cookie") {
+		cookieParser("secret")(req, res, next);
+	} else if (storageMethod === "session") {
+		session({
+			secret: "secret",
+			resave: true,
+			saveUninitialized: true,
+		})(req, res, next);
+	} else {
+		next(new Error("Invalid storage method"));
+	}
+};
+
+// Use the middleware for all routes
+app.use(setupStorage);
 
 app.get("/", (req, res) => {
 	const user = {
@@ -12,12 +31,24 @@ app.get("/", (req, res) => {
 		gender: "Male",
 		role: "admin",
 	};
-	res.cookie("user", user, { signed: true }).send("Hello World!");
+
+	if (storageMethod === "cookie") {
+		res.cookie("user", user, { signed: true }).send("Hello World!");
+	}
+
+	if (storageMethod === "session") {
+		req.session.user = user;
+
+		console.log(req.session);
+		res.send("Hello World!");
+	}
 });
 
 app.get("/user", (req, res) => {
-	if (req.signedCookies.user?.role === "admin") {
-		const user = req.signedCookies.user;
+	console.log(storageMethod);
+
+	if (req[storageMethod].user?.role === "admin") {
+		const user = req[storageMethod].user;
 		res.send(user);
 	} else {
 		res.send("No user found");
@@ -25,7 +56,9 @@ app.get("/user", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-	res.clearCookie("user").send("Logged out");
+	req.storageMethod.destroy(() => {
+		res.send("Logged out");
+	});
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));

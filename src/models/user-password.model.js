f220b2045b7b148c2d8df1344f79e91519255aae
Miguel Chavez <toonchavez8@gmail.com>
2023-12-17 18:42:36 -0600

    Feat: add user password model

---
+++ src/models/user-password.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userPasswordCollection = "userPassword";
+
+const userPasswordSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		ref: "users",
+	},
+	token: {
+		type: String,
+		required: true,
+	},
+	isUsed: {
+		type: Boolean,
+		default: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		expireAfterSeconds: 3600,
+	},
+});
+
+mongoose.set("strictQuery", false);
+
+const userPasswordModel = mongoose.model(
+	userPasswordCollection,
+	userPasswordSchema
+);
+
+export default userPasswordModel;
